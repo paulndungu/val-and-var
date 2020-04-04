5c361f19e24426d8dd2ac961516050122e7a5ede
paulndungu <p.ndungu.kara@gmail.com>
2020-04-04 16:16:05 -0700

    wrote down the whole code

---
+++ src/main.kt
@@ -0,0 +1,17 @@
+fun main(){
+
+//    var-----------------------
+    var name= "Mich"
+    name= "Mich Daniels"
+    var desc= "male,young."
+//    val-----------------------
+    val age= 25
+    val job:String?
+    job= "plumber"
+
+    println(name)
+    println("Age: $age")
+    println("Description: "+desc )
+    println("Job: $job")
+
+    }
